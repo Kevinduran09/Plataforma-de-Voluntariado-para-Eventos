851c88532bdba4f66c46ccd1e2a71ee5c2feb76f
Kevinduran09 <kevinduran.net.123@gmail.com>
2025-04-06 16:44:51 -0600

    add nothing

--- app/features/EventDashboard/pages/EventDashboardPage.tsx
+++ app/features/EventDashboard/pages/EventDashboardPage.tsx
@@ -5,7 +5,7 @@ import '../styles/eventStyle.css';
 import type { Route } from '.react-router/types/app/+types/root';
 import type { Evento } from "../domain/EventDashboard";
 import { GetEventDashboard } from "../useCases/useCaseEventDashboard";
-import useAuthStore from "~/store/useAuthStore";
+
 import InscriptionEvents from "../ui/InscriptionEvents";
 import EventCard from "../ui/EventCard";
 
@@ -19,12 +19,65 @@ export function meta({ }: Route.MetaArgs) {
 }
 
 export async function clientLoader() {
-  const response = await GetEventDashboard();
+  // const response = await GetEventDashboard();
 
-  if (!response) {
-    throw new Response("Error al cargar los eventos", { status: 500 });
-  }
-  return { data: response }
+  // if (!response) {
+  //   throw new Response("Error al cargar los eventos", { status: 500 });
+  // }
+  const eventos = [
+    {
+      id: "1",
+      categoria: "Ambiental",
+      nombre: "Reforestación en el parque",
+      descripcion: "Evento de reforestación para mejorar las áreas verdes.",
+      fecha: "2025-04-10",
+      lugar: "Parque Nacional",
+      voluntarios_requeridos: 20,
+      organizador_id: "1",
+      tareas: [
+        { nombre: "Repartir bolsas y guantes", estado: "completado" },
+        { nombre: "Recolectar plásticos", estado: "pendiente" },
+        { nombre: "Clasificar residuos", estado: "pendiente" }
+      ]
+    },
+    {
+      id: "2",
+      categoria: "Ambiental",
+      nombre: "Limpieza de Playa",
+      descripcion: "Actividad para recolectar desechos y limpiar la costa.",
+      fecha: "2025-04-20",
+      lugar: "Playa Tamarindo",
+      voluntarios_requeridos: 25,
+      organizador_id: "3",
+      tareas: [
+        { nombre: "Repartir bolsas y guantes", estado: "pendiente" },
+        { nombre: "Recolectar plásticos", estado: "pendiente" },
+        { nombre: "Clasificar residuos", estado: "pendiente" }
+      ]
+    },
+    {
+      id: "69c1",
+      nombre: "Limpieza de Playa",
+      fecha: "2025-04-20",
+      time: "09:00",
+      lugar: "Playa del Sol",
+      categoria: "Medio Ambiente",
+      descripcion: "Un evento para limpiar la playa y crear conciencia ambiental.",
+      voluntarios_requeridos: 20,
+      requiredSkills: "Trabajo en equipo, limpieza",
+      optionalSkills: "Primeros auxilios",
+      ageRequirement: "18+",
+      additionalRequirements: "Llevar guantes y protector solar",
+      tareas: [
+        "Recolectar basura",
+        "Separar reciclables",
+        "Asistir a voluntarios"
+      ],
+      image: "base64string_or_filename.jpg"
+    },
+  ];
+
+  return { data: eventos }
 }
 
 
